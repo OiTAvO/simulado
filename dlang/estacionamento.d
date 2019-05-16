b66507c02f4ea7dbbe842e58bb8e6e8026c086df
OiTAvO <otavio.porto@fatec.sp.gov.br>
2019-05-16 08:12:05 -0300

    style: Changed from 2 to 4 spaces indent

--- dlang/estacionamento.d
+++ dlang/estacionamento.d
@@ -1,60 +1,54 @@
-import std.stdio, std.string, std.conv, std.array;
+import std.stdio;
+import std.string;
+import std.conv;
+import std.array;
 
 void main ()
 {
-  int[2][string] plcs;
-  bool[1005] vagas;
-  int C, N, i, vgs, val, aux, tm;
-  string[] cmd;
-  auto bff = appender!string();
+    int[2][string] plcs;
+    int C, N, i, vgs, val, aux, tm;
+    bool[1005] vagas;
+    string[] cmd;
+    auto bff = appender!string();
 
-  while (readf(" %s %s ", &C, &N))
-  {
-    vagas[] = false;
-    val = 0;
-    vgs = C;
-
-    while (N--)
+    while (readf(" %s %s ", &C, &N))
     {
-      cmd = split(strip(readln()));
-
-      if (cmd[0] == "S")
-      {
-        aux = plcs[cmd[1]][0];
-        vgs += plcs[cmd[1]][1];
-        vagas[aux .. aux + plcs[cmd[1]][1]] = false;
-      }
-      else
-      {
-    	tm = to!short(cmd[2]);
-
-    	if (vgs >= tm)
+        vagas[] = false;
+        val = 0;
+        vgs = C;
+        while (N--)
         {
-          for (aux = i = 1; i <= C; ++i)
-          {
-            if (vagas[i])
+            cmd = split(strip(readln()));
+            if (cmd[0] == "S")
             {
-              aux = 1;
+                aux = plcs[cmd[1]][0];
+                vgs += plcs[cmd[1]][1];
+                vagas[aux .. aux + plcs[cmd[1]][1]] = false;
             }
-            else if(aux++ == tm)
+            else
             {
-               plcs[cmd[1]] = [i - tm + 1, tm];
-               val += 10;
-               vagas[plcs[cmd[1]][0] .. i + 1] = true;
-               vgs -= tm;
-               break;
+                tm = to!short(cmd[2]);
+                if (vgs >= tm)
+                {
+                    for (aux = i = 1; i <= C; ++i)
+                    {
+                        if (vagas[i])
+                        {
+                            aux = 1;
+                        }
+                        else if(aux++ == tm)
+                        {
+                            plcs[cmd[1]] = [i - tm + 1, tm];
+                            vagas[plcs[cmd[1]][0] .. i + 1] = true;
+                            val += 10;
+                            vgs -= tm;
+                            break;
+                        }  // end else
+                    }  // for i
+                }  //  end if
             }  // end else
-
-          }  // for i
-
-        }  //  end if(vagas[i])
-
-      }  // end else
-
-    }  // while (N--)
-
-    bff.put(format("%d\n", val));
-  }  // while(readf)
-
-  write(bff.data);
+        }  // while (N--)
+        bff.put(format("%d\n", val));
+    }  // while(readf)
+    write(bff.data);
 }  // main
